68556cb6a75ddd30d00c78a79be59ff1d33cbd47
Dhruv Paleja <dhruvpaleja088@nmims.in>
2026-03-08 03:04:55 +0530

    feat: ULTRA RESPONSIVE - Perfect on ALL screens!

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import { ThemeProvider } from './context/ThemeContext';
 import AppRouter from './router';
 import SmoothScrollProvider from './components/layout/SmoothScrollProvider';
 import { NetworkStatusBanner } from './components/ui/NetworkStatusBanner';
+import { ResponsiveWrapper } from './components/ui/ResponsiveWrapper';
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark">
       <AuthProvider>
-        <SmoothScrollProvider>
-          {/* Network status banner for low connectivity */}
-          <NetworkStatusBanner />
-          <AppRouter />
-        </SmoothScrollProvider>
+        <ResponsiveWrapper>
+          <SmoothScrollProvider>
+            {/* Network status banner for low connectivity */}
+            <NetworkStatusBanner />
+            <AppRouter />
+          </SmoothScrollProvider>
+        </ResponsiveWrapper>
       </AuthProvider>
     </ThemeProvider>
   );
